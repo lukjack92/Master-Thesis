11ea451b64517298b6379b98ccf152f24aa4852d
lukjack92 <lukjack92@gmail.com>
2019-07-20 23:46:20 +0200

    The modificated upload.php file

--- upload.php
+++ upload.php
@@ -1,3 +1,4 @@
+
 <?php
 
 session_start();
@@ -5,36 +6,31 @@ require_once "conf_db/config.php";
 require_once 'func_msg/functions.php';
 
 if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true){
-	header("Location: login.php");
-	exit;
+        header("Location: login.php");
+        exit;
 } else {
-	$file = $_FILES['fileToUpload']['name'];
-	
-	if($_FILES['fileToUpload']['name'] != "") {
-		try {
-		if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "upload/upload.txt")) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-		
-		//Operation on the file
-		$file = fopen("upload/upload.txt", "r");
-		
-		while(!feof($file)) {
-			$content = fgets($file);
-			$carray = explode(";",$content);
-			list($question,$ansa,$ansb,$ansc,$ansd,$odp) = $carray;
-			$sql = "INSERT INTO `questions` (`question`, `ansa`, `ansb`, `ansc`, `ansd`, `odp`) VALUES ('$question', '$ansa', '$ansb', '$ansc', '$ansd', '$odp')";
-			@mysqli_query($link,$sql);
-		}
-		fclose($file);
-		
-    } else {
+        $file = $_FILES['fileToUpload']['name'];
+
+        if($_FILES['fileToUpload']['name'] != "") {
+                if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "upload/upload.txt")) {
+
+                //Operation on the file
+                $file = fopen("upload/upload.txt", "r");
+                while(!feof($file)) {
+                        $content = fgets($file);
+                        $carray = explode(";",$content);
+                        list($question,$ansa,$ansb,$ansc,$ansd,$odp) = $carray;
+                        $sql = "INSERT INTO `questions` (`question`, `ansa`, `ansb`, `ansc`, `ansd`, `odp`) VALUES ('$question', '$ansa', '$ansb', '$ansc', '$ansd', '$odp')";
+                        @mysqli_query($link,$sql);
+                }
+                fclose($file);
+        $_SESSION['ok'] = "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        msg_logs_users($_SESSION['login'],"The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.");
+        header("Location: welcome.php");
+        } else {
         echo "Sorry, there was an error uploading your file.";
     }
-	
-	} catch(Exception $e) {
-		echo $e->getMessage();
-		}
-	} else { $_SESSION['ok'] = "Sorry, there was an error uploading your file."; 
-	header("Location: welcome.php");}
+        } else { $_SESSION['ok'] = "Sorry, there was an error uploading your file.";
+        header("Location: welcome.php");}
 }
 ?>
