51a2dee926abe5a61ae4dcdb6f5ede652cb733e2
lukjack92 <lukjack92@gmail.com>
2019-11-01 23:00:21 +0100

    Added error-404.php and downtime.html pages

---
+++ error-404.php
@@ -0,0 +1,88 @@
+<?php
+
+error_reporting(0);
+
+	// Initialize the session
+	session_start();
+	
+		// Check if the user is logged in, if not then redirect him to login page
+	if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true){
+		header("Location: login.php");
+		exit;
+	}
+	
+	/*
+	//Session timeout
+	$time = $_SERVER['REQUEST_TIME'];
+	
+	$timeout_duration = 200;
+	if(isset($_SESSION['LAST_ACTIVITY']) && ($time - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
+		session_unset();
+		session_destroy();
+		session_start();
+		header("Location: index.php");
+		exit;
+	}
+
+	$_SESSION['LAST_ACTIVITY'] = $time;
+	
+	*/
+	
+	require_once "conf_db/config.php";
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+	<link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.4.0/css/bootstrap4-toggle.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Thambi" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Custom styles for this template -->
+
+</head>
+<body>
+
+      <nav class="navbar navbar-expand-lg bg-dark">
+		<a class="navbar-brand" href="logout.php"> <i class="fas fa-home"></i> Logout</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<img src="img/hamb.png" height="15" width="20">
+			</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<!--
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">About<span class="sr-only">(current)</span></a>
+				</li>
+			</ul>
+		-->
+		</div>
+	  </nav>
+
+<div class="container color_white">
+
+<h1>404 - Page Not Found</h1>
+
+</div>
+	<nav class="navbar-fixed-bottom">
+		<div class="footer text-center bg-dark">
+			Copyright &copy; <?php echo date("o"); ?> Designed by Łukasz Jackowski
+			<h6 class="text-danger">This version is in development</h6>
+		</div>
+	</nav>
+	
+	<script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/jquery-autosize@1.18.18/jquery.autosize.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.4.0/js/bootstrap4-toggle.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<!--<script type="text/javascript"src="bootstrap-4.3/js/bootstrap.min.js"></script>
+	<script type="text/javascript"src="http://code.jquery.com/jquery-3.3.1.js"></script>-->
+  	<script type="text/javascript" src="countdown.js"></script>
+	<script type="text/javascript" src="test.js"></script>
+
+</body>
+</html>
