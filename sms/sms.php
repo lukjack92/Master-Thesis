2f6fc55657d08f8d8d8aeb147556688dbb2f95b5
lukjack92 <lukjack92@gmail.com>
2020-05-26 21:43:17 +0200

    Updated_3 file sms.php

--- sms/sms.php
+++ sms/sms.php
@@ -5,17 +5,19 @@ require_once "../func_msg/functions.php";
 require "vendor/autoload.php";
 require "secret_key_sns.php";
 
+$credentials = new Aws\Credentials\Credentials($key, $secretKey);
+
 if(isset($_POST["phoneNumber"])) {
 
 $sdk = new Aws\Sns\SnsClient([
                 'region'  => 'eu-west-1',
                 'version' => 'latest',
-                'credentials' => ['key' => $key, 'secret' => $secretKey]
+                'credentials' => $credentials
         ]);
 
         $six_digit_number = mt_rand(100000, 999999);
 
-        $msg="One Time Password: ".$six_digit_number;
+        $msg="One Time Password is: ".$six_digit_number;
         $number=$_POST["phoneNumber"];
 
         $result = $sdk->publish([
