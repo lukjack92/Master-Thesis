d21bc2807c990a1f2a7e650c26db69db4cf052ba
lukjack92 <lukjack92@gmail.com>
2020-06-17 22:02:00 +0200

    Fixed error-404

--- error-404.php
+++ error-404.php
@@ -17,9 +17,7 @@
 
       <nav class="navbar navbar-expand-lg bg-dark">
 		<a class="navbar-brand" href="logout.php"> <i class="fas fa-home"></i> Logout</a>
-
-		<?php } ?>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> -->
 				<img src="img/hamb.png" height="15" width="20">
 			</button>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
