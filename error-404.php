4591859453e40e059ad0f64877097356929ab4ee
lukjack92 <lukjack92@gmail.com>
2020-06-17 21:53:31 +0200

    Fixed error-404

--- error-404.php
+++ error-404.php
@@ -1,14 +1,3 @@
-<?php
-
-	// Initialize the session
-	session_start();
-	
-	// Check if the user is logged in, if not then redirect him to login page
-	if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true || (!isset($_SESSION["loggedInApp"])) || $_SESSION["loggedInApp"] !== true){
-		header("Location: index.php");
-		exit;
-	}
-?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
